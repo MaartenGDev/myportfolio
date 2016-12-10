cf6a7edebec3d8b48eb2014550e1ab77f55a8bef
MaartenGDev <maarten.grootoonk@gmail.com>
2016-12-10 17:49:09 +0100

    Added project cards

--- public/js/app.bundle.js
+++ public/js/app.bundle.js
@@ -44,25 +44,19 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var Typer = __webpack_require__(1);
+	var Hightlight = __webpack_require__(2);
 
 	var typer = new Typer();
-	var content = [{
-	    text: 'Software',
-	    selector: '.profile__tag--type',
-	    time: 50
-	}, {
-	    text: ' ',
-	    selector: '.profile__tag--spacing',
-	    time: 0
-	}, {
-	    text: 'Developer',
-	    selector: '.profile__tag--branch',
-	    time: 80
-	}];
+	var highlight = new Hightlight();
+
+	var content = [{ text: 'Software', selector: '.profile__tag--type', time: 50 }, { text: ' ', selector: '.profile__tag--spacing', time: 0 }, { text: 'Developer', selector: '.profile__tag--branch', time: 80 }];
+
 	typer.type(content);
+
+	highlight.init('.project', [{ selector: '.project__image', classes: ['--zoomed', '--preview'] }, { selector: '.project__overlay', classes: ['--smooth', '--dark'] }, { selector: '.project__chip', classes: ['--hidden', '--zoomed'] }, { selector: '.project__view', classes: ['--hidden'] }]);
 
 /***/ },
 /* 1 */
@@ -145,6 +139,62 @@
 	}();
 
 	module.exports = Typed;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	var Highlight = function () {
+	    function Highlight() {
+	        _classCallCheck(this, Highlight);
+	    }
+
+	    _createClass(Highlight, [{
+	        key: "init",
+	        value: function init(selector, actions) {
+	            var _this = this;
+
+	            var elements = document.querySelectorAll(selector);
+
+	            elements.forEach(function (element) {
+	                element.addEventListener("mouseenter", function () {
+	                    return _this.handleHover(element, actions);
+	                });
+	                element.addEventListener("mouseleave", function () {
+	                    return _this.handleHover(element, actions);
+	                });
+	            });
+	        }
+	    }, {
+	        key: "handleHover",
+	        value: function handleHover(element, actions) {
+	            console.log(element);
+	            console.log(actions);
+	            actions.forEach(function (action) {
+	                var elements = element.querySelectorAll(action.selector);
+
+	                elements.forEach(function (target) {
+	                    action.classes.forEach(function (modifier) {
+	                        var selector = action.selector.substr(1);
+	                        var className = selector + modifier;
+
+	                        target.classList.toggle(className);
+	                    });
+	                });
+	            });
+	        }
+	    }]);
+
+	    return Highlight;
+	}();
+
+	module.exports = Highlight;
 
 /***/ }
 /******/ ]);
--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,22 @@
 const Typer = require("./typer.js");
+const Hightlight = require("./highlight");
 
 const typer = new Typer;
+const highlight = new Hightlight;
+
 const content = [
-    {
-        text: 'Software',
-        selector: '.profile__tag--type',
-        time: 50
-    },
-    {
-        text: ' ',
-        selector: '.profile__tag--spacing',
-        time: 0
-    },
-    {
-        text: 'Developer',
-        selector: '.profile__tag--branch',
-        time: 80
-    }
+    {text: 'Software', selector: '.profile__tag--type', time: 50},
+    {text: ' ', selector: '.profile__tag--spacing', time: 0},
+    {text: 'Developer', selector: '.profile__tag--branch', time: 80}
 ];
+
 typer.type(content);
+
+highlight.init('.project', [
+    {selector: '.project__image', classes: ['--zoomed', '--preview']},
+    {selector: '.project__overlay', classes: ['--smooth','--dark']},
+    {selector: '.project__chip', classes: ['--hidden','--zoomed']},
+    {selector: '.project__view', classes: ['--hidden']},
+]);
+
+
