e167e1cdf74e889d7a4b9d3e4b2cfcbd93114c89
MaartenGDev <maarten.grootoonk@gmail.com>
2017-02-03 21:28:36 +0100

    Added dynamic project list

--- app.js
+++ app.js
@@ -1,20 +1,62 @@
 const express = require('express')
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const app = express()
+const helmet = require('helmet')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.use('/.well-known/acme-challenge/', express.static(__dirname + '/.well-known/acme-challenge'));
+app.use(helmet())
 
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
+const projects = [
+    {
+        image: 'images/project__joblist.png',
+        title: 'JobList',
+        tags: ['PHP', 'Material Design', 'Laravel', 'Landing Page'],
+        url: 'https://github.com/MaartenGDev/JobList',
+    },
+    {
+        image: 'images/project__tripsearch.png',
+        title: 'TripSearch',
+        tags: ['ElasticSearch', 'GuzzleHttp', 'Composer', 'PHP'],
+        url: 'https://github.com/MaartenGDev/TripSearch',
+    },
+    {
+        image: 'images/project__cars.png',
+        title: 'Cars',
+        tags: ['PHP', 'Symfony', 'Material Design', 'UX'],
+        url: '#projects',
+    },
+    {
+        image: 'images/project__cachedrivers.png',
+        title: 'CacheDrivers',
+        tags: ['PHPUnit', 'Travis CI', 'Package', 'Code Coverage'],
+        url: 'https://github.com/MaartenGDev/CacheDrivers',
+    },
+    {
+        image: 'images/project__planner.png',
+        title: 'Planner',
+        tags: ['ReactJS', 'SPA', 'Laravel', 'REST API'],
+        url: 'https://github.com/MaartenGDev/planner',
+    },
+    {
+        image: 'images/project__devblog.png',
+        title: 'devBlog',
+        tags: ['Laravel', 'Blog', 'CRUD'],
+        url: 'https://github.com/MaartenGDev/devBlog',
+    },
+
+];
+
+app.get('/', (req, res) => {
     res.render('home', {
-        name: 'maarten'
+        name: 'maarten',
+        projects: projects
     })
 })
 
-app.listen(3001, function () {
+app.listen(3001, _ => {
     console.log('Example app listening on port 3001!')
 })
