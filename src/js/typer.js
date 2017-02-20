732164d364d2c106d0eddc2b67d767f292a24056
MaartenGDev <maarten.grootoonk@gmail.com>
2017-02-20 18:08:32 +0100

    Moved to sass

--- src/js/typer.js
+++ src/js/typer.js
@@ -1,44 +1,43 @@
 class Typed {
     sleep(time) {
-        return new Promise((resolve) => setTimeout(resolve, time));
+        return new Promise(resolve => setTimeout(resolve, time));
     }
 
     type(elements) {
         let index = 0;
 
-        let nextItem = (index) => {
-            const {selector, text, time} = elements[index];
+        let nextItem = index => {
+            const { selector, text, time } = elements[index];
             const letters = text.split("");
 
             const element = document.querySelector(selector);
 
-            this.loopThroughLetters(element, letters, time).then(() => {
+            this.loopThroughLetters(element, letters, time).then(_ => {
                 index++;
 
-                if(index < elements.length){
+                if (index < elements.length) {
                     nextItem(index)
                 }
             });
         };
 
         nextItem(index)
-
     }
 
     loopThroughLetters(element, letters, time) {
-        return new Promise(res => {
+        return new Promise(resolve => {
             let index = 0;
 
             const nextItem = (index) => {
-                this.sleep(time).then(() => {
+                this.sleep(time).then(_ => {
                     element.innerHTML += letters[index];
 
                     index++;
 
-                    if(index < letters.length){
+                    if (index < letters.length) {
                         nextItem(index);
-                    }else{
-                        res();
+                    } else {
+                        resolve();
                     }
                 })
             };
