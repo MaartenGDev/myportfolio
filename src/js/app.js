098309f75f77329d3b6e31df5f1f339bce4f29bc
MaartenGDev <maarten.grootoonk@gmail.com>
2017-05-21 21:13:38 +0200

    Removed unused js files

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,1 @@
-import Highlight from './highlight'
-import Tabs from './tabs'
-import staticDataLoader from './staticDataLoader'
 import '../sass/app.sass'
-
-const highlight = new Highlight()
-const tabs = new Tabs()
-
-staticDataLoader.load()
-
-highlight.init('.project', [
-  {selector: '.project__image', classes: ['--zoomed', '--preview']},
-  {selector: '.project__overlay', classes: ['--smooth', '--dark']},
-  {selector: '.project__chip', classes: ['--hidden', '--zoomed']},
-  {selector: '.project__view', classes: ['--hidden']}
-])
-
-tabs.init('.tabs__nav')
